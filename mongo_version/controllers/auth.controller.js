cbd4580f1dc390cb15306718dae13a3d5fac9529
Suman Shrestha <sumanstha0614@gmail.com>
2026-05-02 20:06:17 +0545

    03 (MONGO VERSION) :: sign-in route and gets all user and specific user by id routes ready with authorization middleware

--- mongo_version/controllers/auth.controller.js
+++ mongo_version/controllers/auth.controller.js
@@ -3,6 +3,8 @@ import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.js";
+
+
 const signUp = async (req, res, next) => {
   const session = await mongoose.startSession();
   session.startTransaction();
@@ -53,7 +55,45 @@ const signUp = async (req, res, next) => {
   }
 };
 
-const signIn = async (req, res, next) => {};
+const signIn = async (req, res, next) => {
+  try {
+    const {email,password} = req.body;
+    if (!email || !password) {
+      throw new Error("All fields are required");
+    }
+
+    const user = await User.findOne({email});
+
+    if(!user){
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    const isPasswordValid = await bcrypt.compare(password , user.password);
+
+    if(!isPasswordValid){
+      const error = new Error("Invalid password");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const token = jwt.sign({userId : user._id},JWT_SECRET,{expiresIn : JWT_EXPIRES_IN});
+
+    res.status(200).json({
+      success : true,
+      message : "User signed in successfully",
+      data:{
+        token,
+        user
+      }
+    })
+    
+
+  } catch (error) {
+    next(error);
+  }
+};
 
 const signOut = async (req, res, next) => {};
 
--- mongo_version/routes/user.routes.js
+++ mongo_version/routes/user.routes.js
@@ -1,13 +1,10 @@
 import { Router } from "express";
-
+import usersController from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => {
-  res.send({ message: "GET all users" });
-});
-userRouter.get("/:id", (req, res) => {
-  res.send({ message: "GET one user" });
-});
+userRouter.get("/", usersController.getUsers);
+userRouter.get("/:id", authorize ,usersController.getUser);
 userRouter.post("/", (req, res) => {
   res.send({ message: "POST user" });
 });
